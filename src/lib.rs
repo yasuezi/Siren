b288a9ef0c0ad8574032696a8e8ca4582a45acbf
yasuezi <senia@email.binhnamphong.edu.vn>
2025-04-22 02:37:14 +0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -61,7 +61,7 @@ async fn tunnel(req: Request, mut cx: RouteContext<Config>) -> Result<Response> 
         
         if proxy_kv_str.len() == 0 {
             console_log!("getting proxy kv from github...");
-            let req = Fetch::Url(Url::parse("https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/kvProxyList.json")?);
+            let req = Fetch::Url(Url::parse("https://raw.githubusercontent.com/yasuezi/Siren/refs/heads/master/kvProxyList.json")?);
             let mut res = req.send().await?;
             if res.status_code() == 200 {
                 proxy_kv_str = res.text().await?.to_string();
